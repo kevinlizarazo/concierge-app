ff459a913efcac5951ccd6bda0011cd1693e89bb
Kevin Lizarazo <klizarazo@gmail.com>
2023-05-22 11:08:49 -0400

    task/remove-scopes Remove all unused scopes re: Spotify

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,18 +4,8 @@ import SpotifyProvider from "next-auth/providers/spotify";
 const SCOPES = [
   "user-read-email",
   "user-read-private",
-  "user-read-playback-state",
-  "user-modify-playback-state",
-  "user-read-currently-playing",
-  "user-read-recently-played",
   "user-top-read",
-  "user-library-read",
-  "playlist-read-private",
-  "playlist-read-collaborative",
-  "playlist-modify-public",
-  "playlist-modify-private",
-  "user-follow-read",
-  "user-follow-modify",
+
 ].join("+");
 
 export default NextAuth({
